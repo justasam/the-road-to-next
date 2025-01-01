f80623899b5fcf2de999545f61be0e8cf8a7126c
Justas <samuolis.just@gmail.com>
2025-01-02 00:54:33 +0200

    Use shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 
+import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 import "./globals.css";
@@ -33,13 +35,20 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              <LucideKanban />
+              <h1 className="text-lg font-semibold">TicketBounty</h1>
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
-              Tickets
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: "default" })}
+            >
+              <h2 className="font-semibold">Tickets</h2>
             </Link>
           </div>
         </nav>
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,21 @@
-import clsx from "clsx";
+import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
 import Link from "next/link";
 
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
 const TICKET_ICONS = {
-  OPEN: "🔓",
-  IN_PROGRESS: "🔧",
-  DONE: "✅",
+  OPEN: <LucideFileText />,
+  IN_PROGRESS: <LucidePencil />,
+  DONE: <LucideCircleCheck />,
 };
 
 const TicketsPage = () => {
@@ -19,27 +27,25 @@ const TicketsPage = () => {
           All your tickets in one place
         </p>
       </div>
-
+      <Separator />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <div
-            key={ticket.id}
-            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
-          >
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-            <p
-              className={clsx("text-sm text-slate-500 truncate", {
-                "line-through": ticket.status === "DONE",
-              })}
-            >
-              {ticket.content}
-            </p>
-
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2">
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <h3 className="truncate">{ticket.title}</h3>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="line-clamp-3">{ticket.content}</p>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
